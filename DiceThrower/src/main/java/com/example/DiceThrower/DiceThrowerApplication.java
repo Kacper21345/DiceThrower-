96771f9e3feae24fa76f331c35ade6920086554c
Kacper21345 <110726961+Kacper21345@users.noreply.github.com>
2023-10-25 16:46:38 +0200

    Main Code

---
+++ DiceThrower/src/main/java/com/example/DiceThrower/DiceThrowerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiceThrower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceThrowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceThrowerApplication.class, args);
+	}
+
+}
